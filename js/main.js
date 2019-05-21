b09fc9af470b17326fdadf94bba41d6013ecdd8e
Dockstader <c_dockstader@tss-hr420-dm07.fanshawec.ca>
2019-05-21 17:44:00 -0400

    work

--- js/main.js
+++ js/main.js
@@ -8,20 +8,30 @@
   // and using a css selector to find the element we want to use.
 
   //let stores the reference to element as a variable (in memory)
-  let svgGraphic = document.querySelector("#badgeSVG"),
-    mainHeadline = document.querySelector(".main-headline");
-    subHead = document.querySelector("#raster-vector h3"),
-    swapTextButton = document.querySelector(".switch-type");
+  let mainHeadline = document.querySelector(".main-headline");
+      subHead = document.querySelector("#raster-vector h3"),
+      swapTextButton = document.querySelector(".switch-type");
+      allImages = document.querySelectorAll("#image-container img");
+      objectButton = document.querySelector
 
-// functiona re reusable peices of code
+    #image-container img {
+      cursor: pointer;
+    }
+
+// functiona re reusable peices of code- put these between the variables and the event handling
 // that you can run any time
 function logMyId() {
   console.log(this.id);
+  // toggle a class here with classlist
+}
 
 this.style.opacity = 0.5;
 }
 
 function swapText() {
+  console.log(this.previousElementsibling);
+}
+
   mainHeadline.textcontent = "Now you're something else!";
   mainHeadline.classList.Toggle("seleted");
 
@@ -30,6 +40,12 @@ function swapText() {
 }
 
 //events always go here
-svgGraphic.addEventListener("click", logMyId);
 swapTextButton.addEventListener("mouseover", swapText);
+
+// select and loop through a bunch of items at once -> one to many relationships
+  allImages.forEach(item => {
+    item.addEventListener("click", logMyId);
+  });
+
+  objectbutton.addEventListener("cllick")
 })();
